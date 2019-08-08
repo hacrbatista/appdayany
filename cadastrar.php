9fa39cd058f48089685f9e60099c3e2ccd88ccd7
Henrique Batista <hacrbatista@gmail.com>
2019-08-08 18:38:33 -0300

    Ajustes no cadastro

--- cadastrar.php
+++ cadastrar.php
@@ -3,19 +3,29 @@ session_start();
 
 require 'config.php';
 
+$msg = false;
 if(empty($_POST['email']) == false) {
 	$nome = addslashes($_POST['nome']);
 	$email = addslashes($_POST['email']);
 	$senha = md5(addslashes($_POST['senha']));
 
-	$sql = "INSERT INTO usuarios SET email = :email, senha = :senha, nome = :nome";
-	$sql = $pdo->prepare($sql);
-	$sql->bindValue(':email', $email);
-	$sql->bindValue(':senha', $senha);
-	$sql->bindValue(':nome', $nome);
+	$sql = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
+	$sql->bindValue(":email", $email);
 	$sql->execute();
 
-	header("Location: index.php");
+	if($sql->rowCount() == 0) {
+
+		$sql = "INSERT INTO usuarios SET email = :email, senha = :senha, nome = :nome";
+		$sql = $pdo->prepare($sql);
+		$sql->bindValue(':email', $email);
+		$sql->bindValue(':senha', $senha);
+		$sql->bindValue(':nome', $nome);
+		$sql->execute();
+
+		header("Location: login.php");
+	} else {
+		$msg = 'existe';
+	}
 
 }
 
@@ -35,6 +45,12 @@ if(empty($_POST['email']) == false) {
 		<div class="row">
 			<div class="area-total">
 				<h2>Faça o seu cadastro:</h2><br>
+				<?php if($msg === 'existe'){ ?>
+				<div class="alert alert-danger fade in">
+					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+					Login <strong><?php echo $email;?></strong> já cadastrado, usar outro!
+				</div>
+				<?php }?>
 				<form method="POST">
 					<div class="form-group">
 						<input type="text" class="form-control" name="nome" placeholder="Nome:" required>
@@ -46,7 +62,7 @@ if(empty($_POST['email']) == false) {
 						<input type="password" class="form-control" name="senha" placeholder="Senha:" required>
 					</div>
 					<input type="submit" name="enviar" value="Cadastrar" class="btn btn-success botao-cadastrar">
-					<a href="index.php" class="btn btn-danger botao-cancelar">Cancelar</a>
+					<a href="login.php" class="btn btn-danger botao-cancelar">Cancelar</a>
 				</form>
 			</div>
 		</div>
